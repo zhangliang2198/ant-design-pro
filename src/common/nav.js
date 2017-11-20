4e802b1ac8dbc9e5fe9e9b9ab4bf52f3d66e7d7a
nikogu <644506165@qq.com>
2017-11-20 14:38:08 +0800

    fixed import

--- src/common/nav.js
+++ src/common/nav.js
@@ -4,13 +4,13 @@ import dynamic from 'dva/dynamic';
 const dynamicWrapper = (app, models, component) => dynamic({
   app,
   models: () => models.map(m => import(`../models/${m}.js`)),
-  component: () => component,
+  component,
 });
 
 // nav data
 export const getNavData = app => [
   {
-    component: dynamicWrapper(app, ['user'], import('../layouts/BasicLayout')),
+    component: dynamicWrapper(app, ['user'], () => import('../layouts/BasicLayout')),
     layout: 'BasicLayout',
     name: '首页', // for breadcrumb
     path: '/',
@@ -18,7 +18,7 @@ export const getNavData = app => [
       {
         name: '查询表格',
         path: 'table-list',
-        component: dynamicWrapper(app, ['rule'], import('../routes/List/TableList')),
+        component: dynamicWrapper(app, ['rule'], () => import('../routes/List/TableList')),
       },
     ],
   },
