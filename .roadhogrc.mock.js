5553c795e7340888ebcf661ca9ec48b9eaddfb88
ddcat1115 <ddcat1115@gmail.com>
2017-11-22 14:25:56 +0800

    update Login component & reactor Login page

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -69,8 +69,8 @@ const proxy = {
   'GET /api/profile/basic': getProfileBasicData,
   'GET /api/profile/advanced': getProfileAdvancedData,
   'POST /api/login/account': (req, res) => {
-    const { password, userName } = req.body;
-    res.send({ status: password === '888888' && userName === 'admin' ? 'ok' : 'error', type: 'account' });
+    const { password, username } = req.body;
+    res.send({ status: password === '888888' && username === 'admin' ? 'ok' : 'error', type: 'account' });
   },
   'POST /api/login/mobile': (req, res) => {
     res.send({ status: 'ok', type: 'mobile' });
